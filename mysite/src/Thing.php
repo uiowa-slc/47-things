969ddd768ee490b4dfdddfb2e10e003dc16453e3
Dustin Quam <quamsta@gmail.com>
2021-09-23 11:30:36 -0500

    updated modules

--- mysite/src/Thing.php
+++ mysite/src/Thing.php
@@ -5,6 +5,8 @@ use SilverStripe\Forms\TextField;
 use SilverStripe\Assets\Image;
 use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
 use SilverStripe\AssetAdmin\Forms\UploadField;
+use SilverStripe\ORM\ArrayList;
+
 
 class Thing extends DataObject
 {
@@ -37,4 +39,51 @@ class Thing extends DataObject
 
             return $fields;
         }
+    public function ImageLookup(){
+        $page = $this->data();
+        $images = new ArrayList();
+
+        $tries = array(
+
+            'FeaturedImage',
+            'MainImage',
+            'HeaderImage',
+            'Photo',
+            'BackgroundImage',
+            'HeroImage',
+            'MainPhoto',
+            'ProgramPhoto',
+            'Image',
+            'StaffPhoto',
+            'TestimonialPhoto',
+            'Program1Photo',
+            'Program2Photo',
+            'Program3Photo',
+            'DefaultPhoto',
+            'AboutUsFeature1Photo',
+            'AboutUsFeature2Photo'
+
+        );
+
+        //Try the above image fields
+        foreach($tries as $t) {
+            // echo $t;
+            $i = $page::getSchema()->hasOneComponent($page, $t);
+            // echo $i;
+            if($i) {
+                if($page->getComponent($t)->exists()){
+                    // echo 'component exists: '.$i;
+                    $images->push($page->getComponent($t));
+                }
+            }
+        }
+
+
+
+
+
+
+        return $images;
+
+    }
 }
